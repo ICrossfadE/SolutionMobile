3fe536f6d68ca3dcd7cd55d63bc7aeec5777b562
Maks <Crossfade143@gmail.com>
2022-07-19 20:29:35 +0300

    add show window

--- src/client/modules/index.ts
+++ src/client/modules/index.ts
@@ -1,1 +1,23 @@
-// alert('Hello World')
+const startButton: HTMLButtonElement = document.querySelector('.start-booton');
+const submitButton: HTMLButtonElement = document.querySelector('.submit-booton');
+const modalWindow: HTMLDivElement = document.querySelector('.modal_window');
+
+function showPlayWindow(elem: HTMLElement) {
+    elem.classList.add('active');
+    elem.classList.remove('hidden');
+}
+
+function hiddenPlayWindow(elem: HTMLElement) {
+    elem.classList.add('hidden');
+    elem.classList.remove('active');
+}
+
+startButton.addEventListener('click', (e) => {
+    console.log('start');
+    showPlayWindow(modalWindow);
+})
+
+submitButton.addEventListener('click', (e) => {
+    console.log('start');
+    hiddenPlayWindow(modalWindow);
+})
