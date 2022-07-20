0ae179b821e670a7b3051df21d80483b5764ee42
Maks <Crossfade143@gmail.com>
2022-07-20 15:49:20 +0300

    adaptive modal window

--- src/client/modules/index.ts
+++ src/client/modules/index.ts
@@ -1,23 +1,28 @@
 const startButton: HTMLButtonElement = document.querySelector('.start-booton');
 const submitButton: HTMLButtonElement = document.querySelector('.submit-booton');
 const modalWindow: HTMLDivElement = document.querySelector('.modal_window');
+const bacgroundModalWindow: HTMLDivElement = document.querySelector('.modal_window-background');
 
-function showPlayWindow(elem: HTMLElement) {
-    elem.classList.add('active');
-    elem.classList.remove('hidden');
+function showPlayWindow(elem1: HTMLElement, elem2: HTMLElement) {
+    elem1.classList.add('active');
+    elem1.classList.remove('hidden');
+    elem2.classList.add('active');
+    elem2.classList.remove('hidden');
 }
 
-function hiddenPlayWindow(elem: HTMLElement) {
-    elem.classList.add('hidden');
-    elem.classList.remove('active');
+function hiddenPlayWindow(elem1: HTMLElement, elem2: HTMLElement) {
+    elem1.classList.add('hidden');
+    elem1.classList.remove('active');
+    elem2.classList.add('hidden');
+    elem2.classList.remove('active');
 }
 
 startButton.addEventListener('click', (e) => {
     console.log('start');
-    showPlayWindow(modalWindow);
+    showPlayWindow(modalWindow, bacgroundModalWindow);
 })
 
 submitButton.addEventListener('click', (e) => {
     console.log('start');
-    hiddenPlayWindow(modalWindow);
+    hiddenPlayWindow(modalWindow, bacgroundModalWindow);
 })
