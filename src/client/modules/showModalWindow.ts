e7d5b69d5e15bb0156af1c3724bbd11210eae2d1
Maks <Crossfade143@gmail.com>
2022-07-25 22:37:42 +0300

    editorconfig

--- src/client/modules/showModalWindow.ts
+++ src/client/modules/showModalWindow.ts
@@ -1,13 +1,9 @@
-export function showPlayWindow(elem1: HTMLElement, elem2: HTMLElement) {
-    elem1.classList.add('active');
-    elem1.classList.remove('hidden');
-    elem2.classList.add('active');
-    elem2.classList.remove('hidden');
-};
+export function showPlayWindow(elem: HTMLElement) {
+  elem.classList.add("active");
+  elem.classList.remove("hidden");
+}
 
-export function hiddenPlayWindow(elem1: HTMLElement, elem2: HTMLElement) {
-    elem1.classList.add('hidden');
-    elem1.classList.remove('active');
-    elem2.classList.add('hidden');
-    elem2.classList.remove('active');
-};
+export function hiddenPlayWindow(elem: HTMLElement) {
+  elem.classList.add("hidden");
+  elem.classList.remove("active");
+}
--- src/client/modules/startGame.ts
+++ src/client/modules/startGame.ts
@@ -1,29 +1,24 @@
 //DOM Elements
-import {startButton,
-        submitButton,
-        inputSubmitNumber,
-        modalWindow,
-        bacgroundModalWindow,
-        stringRandomNumbers
-} from './domElements'
+import { startButton, submitButton, inputSubmitNumber, modalWindow, bacgroundModalWindow, stringRandomNumbers } from "./domElements";
 
 //Functions
-import {showPlayWindow, hiddenPlayWindow} from './showModalWindow';
-import {addHtmlText,initStart} from './createNumbers';
+import { showPlayWindow, hiddenPlayWindow } from "./showModalWindow";
+import { addHtmlText, initStart } from "./createNumbers";
 
+startButton.addEventListener("click", (e) => {
+  showPlayWindow(modalWindow);
+  showPlayWindow(bacgroundModalWindow);
 
+  addHtmlText(stringRandomNumbers);
+});
 
-startButton.addEventListener('click', (e) => {
-    showPlayWindow(modalWindow, bacgroundModalWindow);
-    addHtmlText(stringRandomNumbers);
-})
-
-submitButton.addEventListener('click', (e) => {
-    let status = initStart(stringRandomNumbers, inputSubmitNumber);
-    console.log(status);
-    if(status) {
-        inputSubmitNumber.value = ''; 
-    } else {
-        hiddenPlayWindow(modalWindow, bacgroundModalWindow)
-    }
-})
+submitButton.addEventListener("click", (e) => {
+  let status = initStart(stringRandomNumbers, inputSubmitNumber);
+  console.log(status);
+  if (status) {
+    inputSubmitNumber.value = "";
+  } else {
+    hiddenPlayWindow(modalWindow);
+    hiddenPlayWindow(bacgroundModalWindow);
+  }
+});
